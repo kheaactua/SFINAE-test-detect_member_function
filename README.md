d0e4f763347a08629cfe4fea191e0461182c84b1
Matthew Russell <mrussell@neptec.com>
2018-03-26 16:46:14 -0400

    Got simple non-class to work

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SFINAE-test-detect_member_function
+
 SFINAE is annoying.  Making a repo saving working examples of detecting a member function
+
+The code here is mostly based on [SFINAE Hell: detecting template
+methods](http://blog.quasardb.net/sfinae-hell-detecting-template-methods/)
+
+Tested with:
+- clang++ 5.0.1
+- g++ 5.4
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <type_traits>
+
+class NoPPV {
+   public:
+    NoPPV() {
+        std::cout << "No PPV" << std::endl;
+    }
+};
+
+class HasPPV {
+   public:
+    HasPPV() {
+        std::cout << "Has PPV" << std::endl;
+    }
+
+    auto PPV() -> void {};
+};
+
+namespace { // {{{
+
+    template <typename T>
+    struct has_min_ppv
+    {
+		template <class, class> class checker;
+
+		template <typename C>
+		static std::true_type test(checker<C, decltype(&C::PPV)> *);
+
+		template <typename C>
+		static std::false_type test(...);
+
+		typedef decltype(test<T>(nullptr)) type;
+		static const bool value = std::is_same<std::true_type, decltype(test<T>(nullptr))>::value;
+    };
+
+} // }}}
+
+static_assert(has_min_ppv<HasPPV>::value, "error");
+static_assert(!has_min_ppv<NoPPV>::value, "error");
+
+template <typename T>
+auto PrintType() -> typename std::enable_if<has_min_ppv<T>::value, void>::type
+{
+    std::cout << "Choose object WITH ppv" << std::endl;
+}
+
+template <typename T>
+auto PrintType() -> typename std::enable_if<!has_min_ppv<T>::value, void>::type
+{
+    std::cout << "Choose object WITHOUT ppv" << std::endl;
+}
+
+auto main() -> int
+{
+    NoPPV t1;
+    PrintType<decltype(t1)>();
+
+    HasPPV t2;
+    PrintType<decltype(t2)>();
+
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 expandtab ffs=unix,dos : */
